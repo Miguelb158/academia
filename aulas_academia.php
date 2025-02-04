c079587c01c4c96a11567f10953860bdd9f3f412
Miguel <miguelborgess2016@gmail.com>
2025-02-04 00:30:59 -0300

    .

--- aulas_academia.php
+++ aulas_academia.php
@@ -62,7 +62,7 @@ $result = $stmt->get_result();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciamento de Aulas</title>
-    <link rel="stylesheet" href="aulas.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <style>
     body {
